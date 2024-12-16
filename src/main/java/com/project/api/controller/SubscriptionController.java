fbeda2849bce385a56dff5a55038a357c03272d6
jemshMarcel <jemshjms@gmail.com>
2024-12-16 19:07:29 +0300

    add constructor and use @GetMapping

--- src/main/java/com/project/api/controller/SubscriptionController.java
+++ src/main/java/com/project/api/controller/SubscriptionController.java
@@ -1,5 +1,24 @@
 package com.project.api.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.project.api.model.Subscription;
+import com.project.api.service.SubscriptionService;
+
+
+@RestController
+@RequestMapping( path = "api/subscription" )
 public class SubscriptionController {
-  
+  private final SubscriptionService subscriptionService;
+
+  public SubscriptionController(SubscriptionService subscriptionService){
+    this.subscriptionService = subscriptionService;
+  }
+
+  @GetMapping
+  public List<Subscription> getSubscription(){
+    return subscriptionService.getSubscription();
+  }
 }
