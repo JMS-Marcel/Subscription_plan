5c383ddb4f1b4773a0f0ed3cb346366292c38798
jemshMarcel <jemshjms@gmail.com>
2024-12-16 19:06:19 +0300

    init SubscriptionService and add function getSubscription()

--- src/main/java/com/project/api/service/SubscriptionService.java
+++ src/main/java/com/project/api/service/SubscriptionService.java
@@ -1,8 +1,22 @@
 package com.project.api.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.project.api.model.Subscription;
+import com.project.api.repository.SubscriptionRepository;
 
 @Service
 public class SubscriptionService {
-  
+  private final SubscriptionRepository subscriptionRepository;
+
+  public SubscriptionService(SubscriptionRepository subscriptionRepository)
+  {
+    this.subscriptionRepository = subscriptionRepository;
+  }
+
+  public List<Subscription> getSubscription(){
+    return subscriptionRepository.findAll();
+  }
 }
