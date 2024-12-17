bd71eb3def1466a67fc58f1af98b0f9bb59bd9ff
jemshMarcel <jemshjms@gmail.com>
2024-12-17 13:27:24 +0300

    init data and create fuction to getPayment

--- src/main/java/com/project/api/controller/PaymentController.java
+++ src/main/java/com/project/api/controller/PaymentController.java
@@ -1,8 +1,25 @@
 package com.project.api.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+
+import com.project.api.model.Payment;
+import com.project.api.service.PaymentService;
 
 @RestController
+@RequestMapping(path = "api/payment")
 public class PaymentController {
-  
+  private final PaymentService paymentService;
+
+  public PaymentController(PaymentService paymentService){
+    this.paymentService = paymentService;
+  }
+
+  @GetMapping
+  public List<Payment> getPayments(){
+    return paymentService.getPayment();
+  }
+
 }
--- src/main/java/com/project/api/service/PaymentService.java
+++ src/main/java/com/project/api/service/PaymentService.java
@@ -1,8 +1,21 @@
 package com.project.api.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.project.api.model.Payment;
+import com.project.api.repository.PaymentRepository;
 
 @Service
 public class PaymentService {
+  private final PaymentRepository paymentRepository;
   
+  public PaymentService(PaymentRepository paymentRepository){
+    this.paymentRepository = paymentRepository;
+  }
+
+  public List<Payment> getPayment(){
+    return paymentRepository.findAll();
+  }
 }
