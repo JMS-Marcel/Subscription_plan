48aec765fe75b75f15b6a5a2bd5eb1b91a4a04c7
jemshMarcel <jemshjms@gmail.com>
2024-12-17 12:26:25 +0300

    creaate method to create , updat and delete subscription

--- src/main/java/com/project/api/controller/SubscriptionController.java
+++ src/main/java/com/project/api/controller/SubscriptionController.java
@@ -1,7 +1,10 @@
 package com.project.api.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.project.api.model.Subscription;
@@ -20,5 +23,33 @@ public class SubscriptionController {
   @GetMapping
   public List<Subscription> getSubscription(){
     return subscriptionService.getSubscription();
+  }
+
+  @PostMapping()
+  public ResponseEntity<String> createSubscription(@RequestBody Subscription subscription){
+    subscriptionService.addNewSubscription(subscription);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body("{\"message\":\"Subscription added successfully\"}");
+  }
+
+  @PutMapping(path = "{subscriptionId}")
+  public ResponseEntity<String> updateSubscription(
+    @PathVariable("subscriptionId") Long subscriptionId,
+   @RequestParam(required = false) String type, 
+   @RequestParam(required = false) LocalDate startDate, 
+   @RequestParam(required = false) LocalDate endDate,
+   @RequestParam(required = false) String status
+
+   )
+  {
+    subscriptionService.updateSubscription(subscriptionId, type, startDate, endDate, status);
+    return ResponseEntity.status(HttpStatus.CREATED).body("{\"message\":\"Subscription updated successfully\"}");
+  }
+
+  @DeleteMapping(path = "{subscriptionId}")
+  public ResponseEntity<String> deleteSubscription(@PathVariable("subscriptionId") Long subscriptionId){
+    subscriptionService.deleteSubscription(subscriptionId);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body("{\"message\":\"Subscription id : " + subscriptionId + " is deleted successfully\"}");
   }
 }
--- src/main/java/com/project/api/service/SubscriptionService.java
+++ src/main/java/com/project/api/service/SubscriptionService.java
@@ -1,11 +1,18 @@
 package com.project.api.service;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Service;
 
 import com.project.api.model.Subscription;
 import com.project.api.repository.SubscriptionRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class SubscriptionService {
@@ -18,5 +25,47 @@ public class SubscriptionService {
 
   public List<Subscription> getSubscription(){
     return subscriptionRepository.findAll();
+  }
+
+  public void addNewSubscription(Subscription subscription){
+    Optional<Subscription> subscriptionOptional = subscriptionRepository.findByType(subscription.getType());
+    if(subscriptionOptional.isPresent()){
+      throw new IllegalStateException("This Type is already exist");
+    }
+    subscriptionRepository.save(subscription);
+  }
+
+  @Transactional
+  public void updateSubscription(Long suscriptionId, String type,
+                                @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate, 
+                                @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endDate, 
+                                String status){
+    Subscription subscription = subscriptionRepository.findById(suscriptionId).orElseThrow(
+      ()-> new IllegalStateException("Subscription with id " + suscriptionId + " does not exists")
+    );
+    if( type != null && type.length() > 0 && !Objects.equals(subscription.getType(), type)){
+      Optional<Subscription> subscriptionOptional = subscriptionRepository.findByType(type);
+      if(subscriptionOptional.isPresent()){
+        throw new IllegalStateException("This Type is already exist");
+      }
+      subscription.setType(type);
+    }
+    if(startDate != null && !Objects.equals(subscription.getStartDate(), startDate)){
+      subscription.setStartDate(startDate);
+    }
+    if(endDate != null && !Objects.equals(subscription.getEndDate(), endDate)){
+      subscription.setEndDate(endDate);
+    }
+    if(status != null && status.length() > 0 && !Objects.equals(subscription.getStatus(), status)){
+      subscription.setStatus(status);
+    }
+  }
+
+  public void deleteSubscription(Long subscriptionId){
+    boolean exists = subscriptionRepository.existsById(subscriptionId);
+    if(!exists){
+      throw new IllegalStateException("Subscription with id :" + subscriptionId + " does not exists");
+    }
+    subscriptionRepository.deleteById(subscriptionId);
   }
 }
