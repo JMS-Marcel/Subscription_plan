d7779946a528d956c3504510ff31b08917acc5c8
jemshMarcel <jemshjms@gmail.com>
2024-12-17 19:30:01 +0300

    create method getPackage

--- src/main/java/com/project/api/controller/PackageController.java
+++ src/main/java/com/project/api/controller/PackageController.java
@@ -1,5 +1,28 @@
 package com.project.api.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.project.api.service.PackageService;
+
+import com.project.api.model.Package;
+
+
+
+@RestController
+@RequestMapping(path = "api/package")
 public class PackageController {
+  private final PackageService packageService;
+
+  public PackageController(PackageService packageService){
+    this.packageService = packageService;
+  }
+
+  @GetMapping
+  public List<Package> getPackage() {
+      return packageService.getPackage();
+  }
+  
   
 }
--- src/main/java/com/project/api/service/PackageService.java
+++ src/main/java/com/project/api/service/PackageService.java
@@ -1,5 +1,20 @@
 package com.project.api.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.project.api.repository.PackageRepository;
+
+import com.project.api.model.Package;
+@Service
 public class PackageService {
-  
+  private final PackageRepository packageRepository;
+
+  public PackageService(PackageRepository packageRepository){
+    this.packageRepository = packageRepository;
+  }
+  public List<Package> getPackage(){
+    return packageRepository.findAll();
+  }
 }
