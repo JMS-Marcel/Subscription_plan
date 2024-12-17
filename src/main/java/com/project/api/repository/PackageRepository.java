6355b38ffe64c0863209360ac4a178f4be5ba001
jemshMarcel <jemshjms@gmail.com>
2024-12-17 19:01:25 +0300

    create PackageRepositiry

---
+++ src/main/java/com/project/api/repository/PackageRepository.java
@@ -0,0 +1,7 @@
+package com.project.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface  PackageRepository extends JpaRepository<Package, Long> {
+  
+}
