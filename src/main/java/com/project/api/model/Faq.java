49a32dba414411ec1070e5fd9238a4cb5599c03b
jemshMarcel <jemshjms@gmail.com>
2024-12-18 14:03:51 +0300

    Create Faq.java in model

---
+++ src/main/java/com/project/api/model/Faq.java
@@ -0,0 +1,17 @@
+package com.project.api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table
+public class Faq {
+  
+  
+}
