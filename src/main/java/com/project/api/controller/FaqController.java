5c40989d68b02cd0cb6d999c53ae0a144c489e93
jemshMarcel <jemshjms@gmail.com>
2024-12-18 14:56:07 +0300

    create addfaq() and updateFaq method

--- src/main/java/com/project/api/controller/FaqController.java
+++ src/main/java/com/project/api/controller/FaqController.java
@@ -2,9 +2,10 @@ package com.project.api.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 
 import com.project.api.model.Faq;
 import com.project.api.service.FaqService;
@@ -21,5 +22,21 @@ public class FaqController {
   @GetMapping
   public List<Faq> getFaq(){
     return faqService.getFaq();
+  }
+
+  @PostMapping()
+  public ResponseEntity<String> addNewFaq(@RequestBody Faq faq){
+    faqService.addNewFaq(faq);
+    return ResponseEntity.status(HttpStatus.CREATED).body("{\"message\":\"Faq added successfully\"}");
+  }
+
+  @PutMapping(path = "{faqId}")
+  public ResponseEntity<String> updateFaq(
+    @PathVariable("faqId") Long faqId,
+    @RequestParam(required = false) String question,
+    @RequestParam(required = false) String answer
+  ){
+    faqService.updateFaq(faqId, question, answer);
+    return ResponseEntity.status(HttpStatus.CREATED).body("{\"message\":\"Faq updated successfully\"}");
   }
 }
--- src/main/java/com/project/api/service/FaqService.java
+++ src/main/java/com/project/api/service/FaqService.java
@@ -5,8 +5,11 @@ import org.springframework.stereotype.Service;
 import com.project.api.repository.FaqRepository;
 
 import java.util.List;
+import java.util.Objects;
 
 import com.project.api.model.Faq;
+
+import jakarta.transaction.Transactional;
 @Service
 public class FaqService {
   private final FaqRepository faqRepository;
@@ -17,5 +20,23 @@ public class FaqService {
 
   public List<Faq> getFaq(){
     return faqRepository.findAll();
+  }
+
+  public void addNewFaq(Faq faq){
+    faqRepository.save(faq);
+  }
+
+  @Transactional
+  public void updateFaq(Long faqId, String question, String answer){
+    Faq faq = faqRepository.findById(faqId).orElseThrow(
+    ()-> new IllegalStateException("Faq with id " + faqId +" does not exists")
+    );
+
+    if(question != null && question.length() > 0 && !Objects.equals(faq.getQuestion(), question)){
+      faq.setQuestion(question);
+    }
+    if(answer != null && answer.length() > 0 && !Objects.equals(faq.getAnswer(), answer)){
+      faq.setAnswer(answer);
+    }
   }
 }
