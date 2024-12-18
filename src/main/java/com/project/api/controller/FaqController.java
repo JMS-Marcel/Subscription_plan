001ffd7e0c03f7bc0201e1a587c537d1664fe962
jemshMarcel <jemshjms@gmail.com>
2024-12-18 14:25:18 +0300

    create getFaq method

--- src/main/java/com/project/api/controller/FaqController.java
+++ src/main/java/com/project/api/controller/FaqController.java
@@ -1,8 +1,25 @@
 package com.project.api.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.api.model.Faq;
+import com.project.api.service.FaqService;
+
 @RestController
+@RequestMapping(path = "api/faq")
 public class FaqController {
-  
+  private final FaqService faqService;
+
+  public FaqController(FaqService faqService){
+    this.faqService = faqService;
+  }
+
+  @GetMapping
+  public List<Faq> getFaq(){
+    return faqService.getFaq();
+  }
 }
--- src/main/java/com/project/api/service/FaqService.java
+++ src/main/java/com/project/api/service/FaqService.java
@@ -2,7 +2,20 @@ package com.project.api.service;
 
 import org.springframework.stereotype.Service;
 
+import com.project.api.repository.FaqRepository;
+
+import java.util.List;
+
+import com.project.api.model.Faq;
 @Service
 public class FaqService {
-  
+  private final FaqRepository faqRepository;
+
+  public FaqService(FaqRepository faqRepository){
+    this.faqRepository = faqRepository;
+  }
+
+  public List<Faq> getFaq(){
+    return faqRepository.findAll();
+  }
 }
