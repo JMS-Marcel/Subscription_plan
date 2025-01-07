a45cbe75f519b59776cb4ca9168bd0f9788b07ab
jemshMarcel <jemshjms@gmail.com>
2025-01-07 12:46:22 +0300

    refactor Subscription model to use correct User entity and update JPA annotations

--- src/main/java/com/project/api/model/Subscription.java
+++ src/main/java/com/project/api/model/Subscription.java
@@ -4,7 +4,15 @@ package com.project.api.model;
 import java.time.LocalDate;
 import java.util.List;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -35,7 +43,7 @@ public class Subscription {
 
   @ManyToOne
   @JoinColumn( name = "user_id")
-  private Users user;
+  private User user;
 
   @OneToMany(mappedBy = "subscription")
   private List<Payment> payments;
