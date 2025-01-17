67ab7b74d91142c234e8fa77a79a37eb8199b64e
jemshMarcel <jemshjms@gmail.com>
2025-01-17 10:25:16 +0300

    refactor JwtAuthenticationFilter to integrate token validation with TokenRepository

--- src/main/java/com/project/api/config/JwtAuthenticationFilter.java
+++ src/main/java/com/project/api/config/JwtAuthenticationFilter.java
@@ -1,10 +1,10 @@
 package com.project.api.config;
 
-
-
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.project.api.repository.TokenRepository;
 
 import java.io.IOException;
 
@@ -28,36 +28,39 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
   private final UserDetailsService userDetailsService;
 
+  private final TokenRepository tokenRepository;
+
   @Override
   protected void doFilterInternal(
-    @NonNull HttpServletRequest request,
-    @NonNull HttpServletResponse response, 
-    @NonNull FilterChain filterChain
+      @NonNull HttpServletRequest request,
+      @NonNull HttpServletResponse response,
+      @NonNull FilterChain filterChain
 
-    ) throws ServletException, IOException{
-      final String authHeader = request.getHeader("Authorization");
-      final String jwt;
-      final String userEmail;
-      if(authHeader == null || !authHeader.startsWith("Bearer ")){
-        filterChain.doFilter(request, response);
-        return;
-      }
-      jwt = authHeader.substring(7);
-      userEmail = jwtService.extractUsername(jwt);
-      if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
-        UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
-        if(jwtService.isTokenValid(jwt, userDetails)){
-          UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+  ) throws ServletException, IOException {
+    final String authHeader = request.getHeader("Authorization");
+    final String jwt;
+    final String userEmail;
+    if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+      filterChain.doFilter(request, response);
+      return;
+    }
+    jwt = authHeader.substring(7);
+    userEmail = jwtService.extractUsername(jwt);
+    if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+      UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+      var isTokenValid = tokenRepository.findByToken(jwt)
+            .map(t -> !t.isExpired() && !t.isRevoked())
+            .orElse(false);
+      if (jwtService.isTokenValid(jwt, userDetails) && isTokenValid) {
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
             userDetails,
             null,
-            userDetails.getAuthorities()
-          );
-          authToken.setDetails(
-            new WebAuthenticationDetailsSource().buildDetails(request)
-          );
-          SecurityContextHolder.getContext().setAuthentication(authToken);
-        }
+            userDetails.getAuthorities());
+        authToken.setDetails(
+            new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authToken);
       }
-      filterChain.doFilter(request, response);
+    }
+    filterChain.doFilter(request, response);
   }
 }
